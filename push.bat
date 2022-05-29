0017aa3155b468d19487e76f7bdace591a701e30
xianyu <wangyiyun163rky@163.com>
2022-05-29 22:27:02 +0800

    1

--- push.bat
+++ push.bat
@@ -5,7 +5,7 @@ git status
 git add .
 git commit -m "1"
 git pull git@github.com:rrjjyy/-.git
-git push yuanshen
+git push git@github.com:rrjjyy/-.git
 echo 推送成功：【%1%】
 echo "--------End!--------"
 pause
